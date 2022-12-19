fb9bdd4ad83baf9f91bc0597b642293256582a04
r.araslanov <araslanov.08@gmail.com>
2022-12-19 23:23:23 +0500

    готово

---
+++ src/test/java/GitJUnit5_HW_11.java
@@ -0,0 +1,37 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.selector.ByText;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GitJUnit5_HW_11 {
+
+    @BeforeAll
+    static void setUp(){
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+
+    }
+    @Test
+    void JUnit5Tests(){
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".js-wiki-more-pages-link").click();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $("#wiki-pages-box").find(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
+
+
+    }
+
+
+
+}
